9401d7ec5e123e5113884d5eaaff1119cc0cd1c6
zhangpanzhao <zhangpanzhao@okay.cn>
2019-03-15 00:22:01 +0800

    [test] 测试：注入属性、注入方法

--- app/src/main/java/com/dapn/andokay/MainActivity.java
+++ app/src/main/java/com/dapn/andokay/MainActivity.java
@@ -2,12 +2,35 @@ package com.dapn.andokay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dapn.andokay.baselibrary.ioc.OnClick;
+import com.dapn.andokay.baselibrary.ioc.ViewById;
+import com.dapn.andokay.baselibrary.ioc.ViewUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    @ViewById(R.id.test_tv)
+    private TextView mTestTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewUtils.inject(this);
+    }
+
+    @OnClick(R.id.test_tv)
+    public void testClick(View view) {
+        mTestTv.setText("@OnClick");
+        Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.test2_tv)
+    public void testClick2() {
+        int i = 2 / 0;  // 这种异常，不会crash!!!
+        Toast.makeText(this, "onClick2", Toast.LENGTH_SHORT).show();
     }
 }
