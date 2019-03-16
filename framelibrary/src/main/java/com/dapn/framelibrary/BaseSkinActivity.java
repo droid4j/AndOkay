9ef4cf68c9ecad22d2b15a8df9cb76bcb9dec2a8
zhangpanzhao <zhangpanzhao@okay.cn>
2019-03-16 23:12:02 +0800

    [feat] 增加framelibrary层，为后期换肤等功能预留接口

---
+++ framelibrary/src/main/java/com/dapn/framelibrary/BaseSkinActivity.java
@@ -0,0 +1,15 @@
+package com.dapn.framelibrary;
+
+import com.dapn.andokay.baselibrary.base.BaseActivity;
+
+/**
+ * <pre>
+ *     author : per4j
+ *     e-mail : zhangpanzhao@okay.cn
+ *     time   : 2019/03/16
+ *     desc   : 为换肤等功能预留接口
+ *     version: 1.0
+ * </pre>
+ */
+public abstract class BaseSkinActivity extends BaseActivity {
+}
